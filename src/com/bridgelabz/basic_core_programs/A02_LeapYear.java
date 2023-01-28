9a3e38c8e9dbd5d09c9355705c0df1e955be28f5
upendra <upendrasahu1199@gmail.com>
2023-01-28 23:51:45 +0530

    Day - 5 practice problem

--- src/com/bridgelabz/basic_core_programs/A02_LeapYear.java
+++ src/com/bridgelabz/basic_core_programs/A02_LeapYear.java
@@ -1,6 +1,30 @@
-//LEap Year
+//Leap Year
+/* Condition
+1: (year % 100) = 0  and (year % 400) =0
+2: (year % 4) =0 and (year % 100) != 0 (After every 4 year leap year come)
+        */
 
 package com.bridgelabz.basic_core_programs;
 
+
+import java.util.Scanner;
+
 public class A02_LeapYear {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter year : ");
+        int year = sc.nextInt();
+        if (year >= 1000 && year <= 9999) {
+            if ((year % 4) == 0 && (year % 100) != 0) {
+                System.out.println(year + " is a leap year ");
+            } else if ((year % 100) == 0 && (year % 400) == 0) {
+                System.out.println(year + " is a leap year ");
+            } else {
+                System.out.println(year + "is not a leap year");
+            }
+        }else {
+            System.out.println("Enter 4 digit year");
+        }
+    }
 }
