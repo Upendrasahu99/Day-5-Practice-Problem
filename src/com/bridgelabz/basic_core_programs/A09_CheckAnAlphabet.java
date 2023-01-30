716d980737ba5f719d9fcb8eb923bdf9cd7d4790
upendra <upendrasahu1199@gmail.com>
2023-01-30 18:40:57 +0530

    Day 5 : Practice Problem

--- src/com/bridgelabz/basic_core_programs/A09_CheckAnAlphabet.java
+++ src/com/bridgelabz/basic_core_programs/A09_CheckAnAlphabet.java
@@ -2,13 +2,12 @@
 
 package com.bridgelabz.basic_core_programs;
 
-import java.util.Scanner;
 
 public class A09_CheckAnAlphabet {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter Alphabet : ");
-        char c= sc.next().charAt(0); //doubt how to use
+        char c= 'd';
         if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
             System.out.println("Alphabet is a vowel.");
 
